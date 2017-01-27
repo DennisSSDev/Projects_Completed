257dd3454e18a23ce7613750ce95e1072608fa67
dxs4043 <dxs4043@IGMOH1380-08>
2017-01-27 15:20:35 -0500

    The first ICE WITH ACTUAL CODE

--- ICE WITH DICTIONARY/ICE WITH DICTIONARY/Program.cs
+++ ICE WITH DICTIONARY/ICE WITH DICTIONARY/Program.cs
@@ -10,7 +10,39 @@ namespace ICE_WITH_DICTIONARY
     {
         static void Main(string[] args)
         {
+            Dictionary<string , DictionaryWithWords> namesOfStuff = new Dictionary<string, DictionaryWithWords>();
+            DictionaryWithWords thing1 = new DictionaryWithWords();
+            DictionaryWithWords thing2 = new DictionaryWithWords();
+            DictionaryWithWords thing3 = new DictionaryWithWords();
+            DictionaryWithWords thing4 = new DictionaryWithWords();
 
+            thing1.definition = "a device for keeping insurance companies";
+            thing2.definition = "is graceful, well mannered creature commonly fpound in IGME Labs";
+            thing3.definition = "a person who does code very very fast";
+            thing4.definition = "A person who does code very slowly";
+            namesOfStuff.Add("car", thing1);
+            namesOfStuff.Add("cat", thing2);
+            namesOfStuff.Add("professor", thing3);
+            namesOfStuff.Add("me", thing4);
+            string end = "";
+            while(end != "EXIT")
+            {
+                Console.WriteLine("Enter a word to locate: ");
+                end = Console.ReadLine();
+                if (namesOfStuff.ContainsKey(end) == true)
+                {
+                    Console.WriteLine(namesOfStuff[end]);
+                }
+                else if (end == "EXIT")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You done screwed up, type a correct key name");
+                }
+            }
+            Console.WriteLine("Thank you for using this program");
         }
     }
 }
