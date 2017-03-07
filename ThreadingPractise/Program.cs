eee50f6457ff9ebff82560a03fce8d13964fec1d
denni <denni@DESKTOP-TE16FTI>
2017-03-06 22:23:59 -0500

    Done with the last ICE

---
+++ ThreadingPractise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadingPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] newThreadArray = new Thread[100];
+
+            for (int i = 0; i < newThreadArray.Length; i++)
+            {
+                Number a = new Number(i);
+                Thread b = new Thread(a.Print);
+                newThreadArray[i] = b;
+                b.Start();
+            }
+            
+
+        }
+    }
+}
