ed18fd473319f71d9100d29585ac3df8ea7974ae
dxs4043 <dxs4043@IGMOH1380-04>
2017-03-08 14:54:29 -0500

    Threading

---
+++ More with Threading/Cheese.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace More_with_Threading
+{
+    class Cheese
+    {
+        public int Firmness { get; set; }
+        public string Name { get; set; }
+        public float Age { get; set; }
+        
+        public void AgeCheese()
+        {
+            try
+            {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId + ": Age cheese Begin");
+                Thread.Sleep((int)Age * 1000);
+                Console.WriteLine("Age cheese End");
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
