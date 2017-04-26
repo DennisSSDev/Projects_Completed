d120aee0b1f9ee7a3522ee1a14f08fc851f6e7c4
denni <denni@DESKTOP-TE16FTI>
2017-04-25 22:23:14 -0400

    Working on HW4

--- HW4/LinkedList.cs
+++ HW4/LinkedList.cs
@@ -23,24 +23,29 @@ namespace HW4
             }
         }
 
-        public void Add(string data)
+        public void Add(string data)//works fine, not sure if tail == head is neccessary
         {
             Node newNode = new Node(data);
-            if(tail == null)
+            //add a new node when there are no nodes 
+            //add a new node at the end of the list
+            if (head == null)
             {
-                tail = newNode;
-                head = tail;
+                head = newNode;
+                tail = head;
                 counter++;
+                return;
             }
-            else
+            
+            Node current = head;
+            while (current.Next != null)
             {
-                while (tail!=null)
-                {
-                    tail = tail.Next;
-                }
-                tail = newNode;
-                counter++;
+                current = current.Next;
             }
+            tail = newNode;
+            current.Next = newNode;
+            current.Next = tail;
+            counter++;
+            
         }
 
         public void Clear()
@@ -48,15 +53,16 @@ namespace HW4
             tail = null;
             head = tail;
             counter = 0;
-        }
+        }//works fine
 
-        public string GetElement(int index)
+        public string GetElement(int index)//works fine
         {
             Node temp = head;
             int count = 0;
-            while (count != index && temp != null)//think about this 
+            while (count != index)
             {
                 temp = temp.Next;
+                count++;
             }
             if(temp == null)
             {
@@ -70,12 +76,96 @@ namespace HW4
 
         public void Insert(string data, int index)
         {
-            throw new NotImplementedException();
+            int counter1 = 0;
+            Node tempH = head;
+            Node newNode = new Node(data);
+            if(index <=0)//works fine
+            {
+                Node temp = head;
+
+                head = newNode;
+
+                head.Next = temp;
+                counter++;
+                return;
+            }
+            while(tempH != null)
+            {
+                if(counter1 == index)
+                {
+
+                    Node store1 = tempH;
+                    Node store2 = tempH.Next;
+                    Node store3 = tempH.Previous;
+                    tempH = newNode;
+                    tempH.Next = store2;
+                    tempH.Previous = store1;
+                    store1.Previous = store3;
+                    counter++;
+                    return;
+                }
+                tempH = tempH.Next;
+                counter1++;
+            }
+            Add(data);
         }
 
         public string Remove(int index)
         {
-            throw new NotImplementedException();
+            
+            int counter2 = 0;
+            Node tempH = head;
+            if (index < 0 || index > counter)
+            {
+                return null;
+            }
+            else if (index == 0)
+            {
+                if(head != null)
+                {
+                    string storage = tempH.Data;
+
+                    tempH = tempH.Next;
+                    tempH.Previous = null;
+                    counter--;
+                    return storage;
+                }
+                else
+                {
+                    return null;
+                }
+                
+            }
+            while(tempH != null || index != counter2)
+            {
+                tempH = tempH.Next;
+                counter2++;
+            }
+            if(tempH == null)
+            {
+                return null;
+            }
+            if(tempH.Next == null)
+            {
+                string saver = tempH.Data;
+                tail = tempH.Previous;
+                tempH = null;
+                tail.Next = null;
+                counter--;
+                return saver;
+            }
+            else
+            {
+                string saver = tempH.Data;
+                Node right = tempH.Next;//the tailer
+                Node left = tempH.Previous;//the starter
+                tempH = left;
+                tempH.Next = right;
+                counter--;
+                return saver; 
+            }
+                
+            
         }
     }
 }
--- HW4/Program.cs
+++ HW4/Program.cs
@@ -10,6 +10,49 @@ namespace HW4
     {
         static void Main(string[] args)
         {
+            LinkedList listOfNodes = new LinkedList();
+            string commander = null;
+
+            Console.WriteLine("Testing123");
+            while (commander != "quit")
+            {
+                
+                string temp = Console.ReadLine();
+                commander = temp;
+                switch (commander)
+                {
+                    case "q":
+                        commander = "quit";
+                        break;
+                    case "print":
+                        for (int i = 0; i <= listOfNodes.Count; i++)
+                        {
+                            Console.WriteLine(listOfNodes.GetElement(i));
+                        }
+                        break;
+                    case "count":
+                        Console.WriteLine(listOfNodes.Count);
+                        break;
+                    case "clear":
+                        listOfNodes.Clear();
+                        break;
+                    case "remove"://look at linked lisk code first
+                        Random ranRemoval = new Random();
+
+                        listOfNodes.Remove(ranRemoval.Next(0,listOfNodes.Count));
+                        break;
+                    case "scramble"://look at the linked list code first
+                        Random ran = new Random();
+                        Node storedNode = new Node(listOfNodes.Remove(ran.Next(0, listOfNodes.Count+1)));
+                        listOfNodes.Insert(storedNode.Data, ran.Next(0, listOfNodes.Count + 1));
+                        break;
+
+
+                    default:
+                        listOfNodes.Add(commander);
+                        break;
+                }
+            }
         }
     }
 }
